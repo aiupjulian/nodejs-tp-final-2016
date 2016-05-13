7508bbf241453d477f46aa4608b13c6f846811fb
Julian <julianaiup@yahoo.com>
2016-05-12 23:20:04 -0300

    Added employees model test

---
+++ test/unit/db/employees.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Employee = require('../models/employees.js');
+
+describe('Test of employees', function(){
+	before(function(done) {
+		mongoose.connect('mongodb://localhost/nodejs-tp-final-2016', done);
+	})
+
+	it('Should save employee in DB', function(done){
+		var e = new Employee ({
+			firstName: 'Julian',
+			lastName: 'Aiup',
+			email: 'julianaiup@yahoo.com',
+			password: 'julianaiup'
+		});
+		e.save(done);
+	});
+});
