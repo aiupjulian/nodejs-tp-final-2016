ebf1a5f1f439f738db53506e17b37c56d3024934
Julian <julianaiup@yahoo.com>
2016-05-13 01:32:48 -0300

    Added employee create route and list

--- routes/main.js
+++ routes/main.js
@@ -6,3 +6,24 @@ app.get('/panel/employees', function(req, res){
 		res.render('list', { title: 'List', employees: docs });
 	});
 });
+
+app.get('/panel/employees/new', function(req, res){
+	res.render('new', { title: 'New'});
+});
+
+app.post('/panel/employees/new', function(req, res){
+	console.log(req.body);
+	var e = new Employees({
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: req.body.password
+	});
+	e.save(function(err, doc){
+		if(!err){
+			res.redirect('/panel/employees');
+		} else {
+			res.end(err);    
+		}    
+	});
+});
