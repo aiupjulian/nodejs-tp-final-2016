dcc08f2514822f0e3143b17cd7c239462a0dd3d3
Julian <julianaiup@yahoo.com>
2016-05-30 16:42:30 -0300

    Added authentication tests to admins and employees

--- test/unit/db/admins.js
+++ test/unit/db/admins.js
@@ -1,17 +1,37 @@
 var mongoose = require('mongoose');
 var Admin = require('../../../models/admins.js');
+var assert = require('assert');
 
 describe('Test of admins', function(){
     before(function(done) {
         mongoose.connect('mongodb://localhost/nodejs-tp-final-2016', done);
-    })
-    it('Should save admin in DB', function(done){
+    });
+
+    it('Should save admin in DB', function(done) {
         var a = new Admin ({
             email: 'admin@admin.com',
             password: '123456'
         });
         a.save(done);
     });
+
+    it('Should store hashed password', function(done) {
+        var a = new Admin({ password: '123456' });
+        a.save(function(err, doc) {
+            assert.ok(doc.password == 'e10adc3949ba59abbe56e057f20f883e', 'Passwords should match');
+            done();
+        });
+    });
+
+    it('Admin should authenticate', function(done) {
+        var a = new Admin({ email:'admin@admin.com', password: '123456' });
+        a.save(function(err, doc) {
+            assert.ok(a.authenticate('123456') === true, 'OK auth');
+            assert.ok(a.authenticate('12345678') === false, 'OK auth');
+            done();
+        });
+    });
+
     after(function(done) { 
         mongoose.connection.close(done);
     })
--- test/unit/db/employees.js
+++ test/unit/db/employees.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Employee = require('../../../models/employees.js');
+var assert = require('assert');
 
 describe('Test of employees', function(){
 	before(function(done) {
@@ -15,6 +16,23 @@ describe('Test of employees', function(){
 		});
 		e.save(done);
 	});
+
+    it('Should store hashed password', function(done) {
+        var e = new Employee({ password: 'julianaiup' });
+        e.save(function(err, doc) {
+            assert.ok(doc.password == '9ce725958d0cca4651adfd148e388387', 'Passwords should match');
+            done();
+        }); 
+    });
+
+    it('Employee should authenticate', function(done) {
+        var e = new Employee({ email:'julianaiup@yahoo.com', password: 'julianaiup' });
+        e.save(function(err, doc) {
+            assert.ok(e.authenticate('julianaiup') === true, 'OK auth');
+            assert.ok(e.authenticate('julianaiupee') === false, 'OK auth');
+            done();
+        });
+    });
 
     after(function(done) {
         mongoose.connection.close(done);
