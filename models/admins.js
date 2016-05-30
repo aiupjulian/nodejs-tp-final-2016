81ee8d9af07e04ce317ba3ff27c4629c6d1152c9
Julian <julianaiup@yahoo.com>
2016-05-30 16:07:07 -0300

    Added crypto module to admins and employees models to store MD5 hash passwords

--- models/admins.js
+++ models/admins.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 var adminSchema = new Schema({
     email: String,
     password: String
+});
+
+adminSchema.pre("save", function(next) {
+    if(this.isModified('password'))
+        this.password = crypto.createHash('md5').update(this.password).digest("hex");
+    next();
 });
 
 var adminModel = mongoose.model('Admins', adminSchema);
