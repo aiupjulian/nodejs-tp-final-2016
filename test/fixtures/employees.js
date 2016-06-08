c4162b8588591a4d6864b55545a7d68151234d6c
Julian <julianaiup@yahoo.com>
2016-06-08 10:23:23 -0300

    Added more employees to fixtures

--- test/fixtures/employees.js
+++ test/fixtures/employees.js
@@ -1,5 +1,9 @@
 exports.Employees = [
     { firstName: 'Cristian', lastName: 'Sar', email:'cristiansar@gmail.com', password: 'cristiansar' },
     { firstName: 'Maria', lastName: 'Mer', email:'mariamer@hotmail.com', password: 'mariamer' },
-    { firstName: 'Ignacio', lastName: 'Ian', email:'ignacioian@yahoo.com', password: 'ignacioian' }
+    { firstName: 'Ignacio', lastName: 'Ian', email:'ignacioian@yahoo.com', password: 'ignacioian' },
+    { firstName: 'Julian', lastName: 'Per', email:'julianper@yahoo.com', password: 'julianper' },
+    { firstName: 'Julio', lastName: 'Pera', email:'juliopera@yahoo.com', password: 'juliopera' },
+    { firstName: 'Julia', lastName: 'Pera', email:'juliapera@yahoo.com', password: 'juliapera' },
+    { firstName: 'Juliana', lastName: 'Pera', email:'julianapera@yahoo.com', password: 'julianapera' },
 ];
